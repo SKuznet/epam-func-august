f35d32a85ec9160e836de4fbfda81357a6a88f55
skuznet <azon.sk@gmail.com>
2019-08-05 16:56:45 +0300

    add test example

--- src/main/java/lesson1/Main.java
+++ src/main/java/lesson1/Main.java
@@ -1,4 +1,15 @@
 package lesson1;
 
 public class Main {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                System.out.println("I am from another thread!");
+            }
+        });
+        thread.start();
+
+        Thread thread1 = new Thread(() -> System.out.println("I am from another thread!"));
+        thread1.start();
+    }
 }
