ac165cba269c60d16207dc86a6ef87ba86269c5c
Ilya Ugryumov <ilya.ugryumov@mail.ru>
2019-08-09 16:55:52 +0300

    add hw1

---
+++ src/main/java/homework1/Cat.java
@@ -0,0 +1,25 @@
+package homework1;
+
+public class Cat {
+    private String name;
+    private int age;
+    private String colorFur;
+
+    public Cat(String name, int age, String colorFur) {
+        this.name = name;
+        this.age = age;
+        this.colorFur = colorFur;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColorFur() {
+        return colorFur;
+    }
+}
---
+++ src/main/java/homework1/Main.java
@@ -0,0 +1,4 @@
+package homework1;
+
+public class Main {
+}
