5812ea61363faa885d43af4089206e283d3b8972
Fedor_Glushchenko <f.glushenko@gmail.com>
2019-08-09 17:27:35 +0300

    Lesson 2 practice

--- src/test/java/lesson2/FunctionalInterfaceExampliImpl.java
+++
@@ -1,9 +0,0 @@
-package lesson2;
-
-public class FunctionalInterfaceExampliImpl implements FunctionalInterfaceExample {
-
-    @Override
-    public void checkMoney() {
-        System.out.println("Nothing....");
-    }
-}
