754482b4c187265c2f07c604a052358814aedd84
valeriy-worker <valeriy-worker@mail.ru>
2019-08-11 18:17:40 +0300

    home work 1 done

--- src/main/java/hw_01/Cat.java
+++ src/main/java/hw_01/Cat.java
@@ -1,35 +1,16 @@
 package hw_01;
 
 public class Cat {
-    protected static int count;
-    protected int id;
     protected String state;
     protected String name;
 
     public Cat(String name) {
         this.name = name;
-        id = ++count;
     }
 
-    public Cat(String name,String state) {
+    public Cat(String name, String state) {
         this.state = state;
         this.name = name;
-    }
-
-    public static int getCount() {
-        return count;
-    }
-
-    public static void setCount(int count) {
-        Cat.count = count;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -43,8 +24,7 @@ public class Cat {
     @Override
     public String toString() {
         return "Cat{" +
-                "id=" + id +
-                ", state='" + state + '\'' +
+                "state='" + state + '\'' +
                 ", name='" + name + '\'' +
                 '}';
     }
--- src/main/java/hw_01/Main.java
+++ src/main/java/hw_01/Main.java
@@ -3,12 +3,12 @@ package hw_01;
 import hw_01.impl.BadCatFactory;
 import hw_01.impl.GoodCat;
 import hw_01.impl.GoodCatFactory;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.function.BinaryOperator;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class Main {
@@ -18,6 +18,7 @@ public class Main {
     public static void main(String[] args) {
         Factory goodCatFactory = new GoodCatFactory();
         Factory badCatFactory = new BadCatFactory();
+
         goodCat = goodCatFactory.getCat("Vaska");
         badCat = badCatFactory.getCat("Murka");
         doConsumerExample();
@@ -25,8 +26,8 @@ public class Main {
         doUnaryOperatorExample();
         doBinaryOperatorExample();
         doPredicateExample();
+        doSupplierExample();
     }
-
 
     private static void doConsumerExample() {
         Consumer<Cat> consumer = c -> System.out.println(c);
@@ -44,15 +45,18 @@ public class Main {
     }
 
     private static void doBinaryOperatorExample() {
-        BinaryOperator<Cat> binaryOperator = (c, s) -> new Cat(c.getName() + "-" + s.getName());
+        BinaryOperator<Cat> binaryOperator = (c, s) -> new Cat(c.getName() + "-" + s.getName(), "I am child");
         System.out.println(binaryOperator.apply(goodCat, badCat));
     }
 
     private static void doPredicateExample() {
-//        Predicate<Cat> predicate = c-> c.getId();
+        Predicate<Cat> predicate = c -> c.getName().equals("Vaska");
+        System.out.println(goodCat + " is Vaska? --> " + predicate.test(goodCat));
     }
 
-    //TODO: Operator
-    //TODO: Predicate
-    //TODO: Supplier
+    private static void doSupplierExample() {
+        Supplier<Cat> playfulCatFactory = () -> new Cat("Muska", "Let's play together, right now, oh e");
+        Cat depecheModeCat = playfulCatFactory.get();
+        System.out.println(depecheModeCat);
+    }
 }
