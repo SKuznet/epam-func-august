895984f9df5660cf51c9b2b65a47e7a58c0f4140
artemesin <esin.artem5@gmail.com>
2019-08-12 14:42:34 +0300

    HW1

---
+++ src/main/java/HomeWork/CatCreator.java
@@ -0,0 +1,41 @@
+package HomeWork;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class CatCreator {
+
+    public static List<MyCat> catCreator(int countOfCats){
+        List<MyCat> myCatList = new ArrayList<>();
+        List<String> catNames = new ArrayList<>();
+        catNames.add("First_Kitty");
+        catNames.add("Second_Kitty");
+        catNames.add("Third_Kitty");
+        catNames.add("Fourth_Kitty");
+        catNames.add("Fifth_Kitty");
+        catNames.add("Sixth_Kitty");
+        catNames.add("Seventh_Kitty");
+        catNames.add("Eighth_Kitty");
+
+        for (int i = 0; i < countOfCats; i++) {
+            boolean isHerbivore = (i % 3 == 0);
+            String tempName = catNames.get(new Random().nextInt(7));
+            int catAge = new Random().nextInt(20);
+            myCatList.add(new MyCat(tempName, catAge, i, isHerbivore));
+        }
+        
+        return myCatList;
+    }
+
+    public static void catPrinter(List<MyCat> myCatList){
+        for (MyCat mycat: myCatList) {
+            System.out.printf(mycat.toString());
+            System.out.println();
+            System.out.println("__________________________________________");
+        }
+    }
+
+}
+
+
---
+++ src/main/java/HomeWork/MyCat.java
@@ -0,0 +1,57 @@
+package HomeWork;
+
+public class MyCat {
+    private String name;
+    private int age;
+    private int catId;
+    private boolean isHerbivore;
+
+    public MyCat(String name, int age, int catId, boolean isHerbivore) {
+        this.name = name;
+        this.age = age;
+        this.catId = catId;
+        this.isHerbivore = isHerbivore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getCatId() {
+        return catId;
+    }
+
+    public void setCatId(int catId) {
+        this.catId = catId;
+    }
+
+    public boolean isHerbivore() {
+        return isHerbivore;
+    }
+
+    public void setHerbivore(boolean herbivore) {
+        isHerbivore = herbivore;
+    }
+
+    @Override
+    public String toString() {
+        return "MyCat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", catId=" + catId +
+                ", isHerbivore=" + isHerbivore +
+                '}';
+    }
+}
