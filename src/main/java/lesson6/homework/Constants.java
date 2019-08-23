ca710f32a62341330f335ec1cb30c65a70a9b203
Daniil Timoshenko <Zstudent>
2019-08-23 15:47:29 +0300

    Update homework6 add javadoc

--- src/main/java/lesson6/homework/Constants.java
+++ src/main/java/lesson6/homework/Constants.java
@@ -1,5 +1,8 @@
 package lesson6.homework;
 
+/**
+ * Class contain regex constants
+ */
 public class Constants {
     public static final String WEB_ELEMENT = "^(www|http)([\\S\\s]*)$";
 }
--- src/main/java/lesson6/homework/StreamCSVParser.java
+++ src/main/java/lesson6/homework/StreamCSVParser.java
@@ -8,10 +8,18 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.stream.Stream;
 
+/**
+ * Class for parsing .csv files uses streams
+ */
 public class StreamCSVParser {
 
     private Stream<String> stringStream;
 
+    /**
+     * Return stream that represent .csv file
+     * @param filePath
+     * @return
+     */
     public Stream<String> getStreamFromFilePath(String filePath) {
         Path path = Paths.get(filePath);
 
@@ -24,6 +32,11 @@ public class StreamCSVParser {
         return stringStream;
     }
 
+    /**
+     * Return sorted array contains strings started with "www" or "http"
+     * @param stringStream
+     * @return
+     */
     public String[] getSortedArrayContainsWebElements(Stream<String> stringStream) {
         String[] array = stringStream.sorted().filter((s -> s.matches(Constants.WEB_ELEMENT))).toArray(String[]::new);
         Arrays.sort(array, Comparator.naturalOrder());
