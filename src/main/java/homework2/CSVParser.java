78221d58aef47da17cc199c85510d9cf58b853aa
innerdev <innerdev@mail.ru>
2019-08-23 11:01:40 +0300

    javadoc added

--- src/main/java/homework2/CSVParser.java
+++ src/main/java/homework2/CSVParser.java
@@ -6,7 +6,16 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Parses CSV file on given file path. You should parse only one-column csv-files.
+ */
 class CSVParser {
+    /**
+     * One-column CSV-file parser.
+     * @param filePath Path of file to parse.
+     * @return ArrayList of matched strings.
+     * @throws IOException Throws IOException if file not found.
+     */
     static List parse(String filePath) throws IOException {
         return Files.lines(Paths.get(filePath))
                 .filter(s -> s.contains("www.") || s.contains("http://"))
